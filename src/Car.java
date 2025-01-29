14e3a16a66ce40d90c1d44ed7b2721be9984431f
Thomas <thomasszos@icloud.com>
2025-01-29 14:24:50 -0500

    Initial Versions of Car and Vehicle made.

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,22 @@
-public class Car {
+public class Car extends Vehicle{
+
+    private String brand;
+
+    public Car(int numberOfWheels, String color, float engineSize, String fuelType, String brand) {
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
+
+    private void honk(){
+        System.out.println("Honk, honk!");
+    }
+
+    private void displayInfo(){
+        System.out.println(
+                "Number of Wheels " + getNumberOfWheels()
+                + " Brand: " + brand + "Color" + getColor()
+                + " Engine Size " + getEngineSize()
+                + " Fuel Type " + getFuelType()
+        );
+    }
 }
